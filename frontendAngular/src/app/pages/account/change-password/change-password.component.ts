1936cc22dffb3c00fe4856005abe0c4fceef8d50
mahmutmese <sulleymanmese@gmail.com>
2023-10-20 12:01:56 +0300

    change-password-update

--- frontendAngular/src/app/pages/account/change-password/change-password.component.ts
+++ frontendAngular/src/app/pages/account/change-password/change-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-password',
@@ -12,13 +13,35 @@ export class ChangePasswordComponent implements OnInit {
 
   passwordForm: FormGroup;
   
-  constructor(private authService: AuthService, private fb: FormBuilder, private toasts : ToastrService) {
+  constructor(private authService: AuthService, private fb: FormBuilder, private toasts : ToastrService, private router: Router) {
     this.passwordForm = this.fb.group({
       oldPassword: ['', Validators.required],
-      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      newPassword: ['', [Validators.required, Validators.minLength(8), this.customPasswordValidator()]],
       confirmPassword: ['', Validators.required],
     });
    }
+
+   isUppercaseMissing(password: string): boolean {
+    return !/[A-Z]/.test(password);
+  }
+
+  isSpecialCharacterMissing(password: string): boolean {
+    return !/[!@#$%^&*()_+[\]{};':"\|,.<>/?-]/.test(password);
+  }
+
+   customPasswordValidator() {
+    return (control: FormGroup) => {
+      const newPassword = control.value;
+      const hasUppercase = /[A-Z]/.test(newPassword);
+      const hasSpecialCharacter = /[!@#$%^&*()_+[\]{};':"\\|,.<>/?-]/.test(newPassword);
+
+      if (hasUppercase && hasSpecialCharacter) {
+        return null; // Şifre gereksinimlerini karşılıyor.
+      } else {
+        return { passwordInvalid: true }; // Şifre gereksinimlerini karşılamıyor.
+      }
+    };
+  }
 
   onSubmit() {
     if (this.passwordForm.valid) {
@@ -51,29 +74,11 @@ export class ChangePasswordComponent implements OnInit {
               )
             }
           );
+          this.router.navigate(['/pages/dashboard']);
       } else {
         this.passwordForm.setErrors({ passwordMismatch: true });
       }
     }
-
-    // if (this.passwordForm.valid) {
-    //   const oldPassword = this.passwordForm.get('oldPassword').value;
-    //   const newPassword = this.passwordForm.get('newPassword').value;
-    //   const confirmPassword = this.passwordForm.get('confirmPassword').value;
-
-    //   this.authService.updateUserPassword(oldPassword, newPassword, confirmPassword)
-    //     .subscribe(
-    //       (response) => {
-    //         console.log(response + "response");
-            
-    //       },
-    //       (error) => {
-    //         // Hata durumunda burada gerekli işlemleri gerçekleştirin.
-    //         console.log(error + "error");
-            
-    //       }
-    //     );
-    // }
   }
 
   ngOnInit(): void {
