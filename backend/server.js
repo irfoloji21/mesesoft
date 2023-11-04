ac0386f972337e65dda6b353f10946aff3c1065c
İrfan MEŞE <softwaredevelopment2181@gmail.com>
2023-11-04 13:56:39 +0300

    product sayfasında kapanan backende geçici çözüm

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ const server = app.listen(process.env.PORT, () => {
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
+  // console.log(`Shutting down the server for ${err}`);
+  // console.log(`shutting down the server for unhandle promise rejection`);
 
-  server.close(() => {
-    process.exit(1);
-  });
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
