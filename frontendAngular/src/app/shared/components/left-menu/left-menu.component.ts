6fe8d55c7b2f7f1274c18ce7a5f84b08e423d7f1
Enver Yigit <108479529+enveryigit21@users.noreply.github.com>
2023-11-28 17:04:07 +0300

    MobileMenu
    MobileMenu

--- frontendAngular/src/app/shared/components/left-menu/left-menu.component.ts
+++ frontendAngular/src/app/shared/components/left-menu/left-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavService, Menu } from '../../services/nav.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../classes/category';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-left-menu',
@@ -11,26 +12,78 @@ import { Category } from '../../classes/category';
 })
 export class LeftMenuComponent implements OnInit {
 
-  public menuItems: Menu[];
+  public menuItems: Category[] = [];
+  public subCategoryData: any[] = []; 
+  public irfosub: any[] = [];
+  public subCategoryy :any = []
+  megaMenu;
+  active:any
 
-  constructor(private router: Router, public navServices: NavService , public categoryService:CategoryService) {
-    this.categoryService.getCategories().subscribe((data: any) => {
-      if (data.success) {
-        this.menuItems = data.categories;
-        this.menuItems.forEach((category: Category) => {
-          category.megaMenu = true 
-          category.active=false;
-          category.megaMenu= true
-         
+  constructor(private router: Router, public navServices: NavService, public categoryService: CategoryService,private route: ActivatedRoute
+    ,private product:ProductService) {
+      this.categoryService.getCategories().subscribe((data: any) => {
+        if (data.success) {
+          // Tüm kategorileri alın
+          const categories = data.categories;
+          // Kategori listesini döngüye alarak alt kategorileri yükleyin
+          this.loadSubcategories(categories);
+        }
+      });
+      // this.router.events.subscribe((event) => {
+      //   this.navServices.mainMenuToggle = false;
+      // });
+  }
+  ngOnInit(): void {
+    this.menuItems.forEach((menuItem) => {
+      if (menuItem.subcategories) {
+        menuItem.subcategories.forEach((childrenItem) => {
+          // console.log(childrenItem._id, "childrenItem._id")
+          
+          if (childrenItem._id) {
+            const subcategoryId = childrenItem._id;
+            this.getSubcategoriesByItemId(subcategoryId, childrenItem);
+          }
         });
       }
     });
-    this.router.events.subscribe((event) => {
-      this.navServices.mainMenuToggle = false;
+  }
+
+  getSubcategoriesByItemId(itemId: any, childrenItem: any) {
+    this.categoryService.getCategoryById(itemId).subscribe((subcategories) => {
+      console.log(subcategories , "subcategoriesMenu")
+      childrenItem.subcategories = subcategories; // Her alt kategoriye ait alt kategorileri güncelle
+      // console.log(childrenItem.subcategories, "childrenItem.subcategories")
     });
   }
-  ngOnInit(): void {
+
+  loadSubcategories(categories: any[]) {
+    for (const category of categories) {
+   
+      if (category.subcategories) {
+        category.megaMenu = true ;
+        // category.active=false
+        for (const subcategory of category.subcategories) {
+          if (subcategory._id) {
+            const subcategoryId = subcategory._id;
+            
+            this.categoryService.getCategoryById(subcategoryId).subscribe((subCategoryData) => {
+              console.log(subCategoryData , "subCategoryData")
+              // Alt kategori ile yerini değiştirin
+            
+              const index = category.subcategories.findIndex(sub => sub._id === subCategoryData.category._id);
+              if (index >= 0) {
+                category.subcategories[index] = subCategoryData.category;
+              }
+            });
+          }
+        }
+      }
+    }
+    this.menuItems = categories;
+
   }
+
+
 
   leftMenuToggle(): void {
     this.navServices.leftMenuToggle = !this.navServices.leftMenuToggle;
@@ -39,6 +92,43 @@ export class LeftMenuComponent implements OnInit {
   // Click Toggle menu (Mobile)
   toggletNavActive(item) {
     item.active = !item.active;
+  }
+  getSubcategories(id) {
+    this.categoryService.getCategoryById(id).subscribe((data) => {
+      this.irfosub = data.category.subcategories;
+      // console.log(data, "data");
+    });
+  }
+
+  navigateWithQueryParams(menuItem: string, subItem: string, childrenSubItem: any) {
+    this.product.irfan(childrenSubItem._id).subscribe(res => {
+      this.handleSubCategoryResponse(menuItem, subItem, res);
+    });
+  }
+
+  handleSubCategoryResponse(menuItem: string, subItem: string, res: any) {
+    console.log(res, "emsal");
+  
+    if (res.products && res.products.length > 0) {
+      const productIds = res.products.map(product => product._id);
+  
+      const queryParams = {
+        category: menuItem,
+        subcategory: subItem,
+        childrenSubItem: productIds
+      };
+  
+      console.log(queryParams, "mehmet");
+  
+      this.router.navigate(['/shop/collection/left/sidebar'], {
+        relativeTo: this.route,
+        queryParams,
+  
+      });
+    } else {
+      console.error("Ürün bulunamadı.");
+      // Eğer ürün bulunamazsa, istediğiniz gibi bir hata işleme mekanizması ekleyebilirsiniz.
+    }
   }
 
   onHover(menuItem) {
