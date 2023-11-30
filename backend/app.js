563755d000459a0f5ad3adbc63aa7571554f090d
mahmutmese <sulleymanmese@gmail.com>
2023-11-30 13:34:33 +0300

    images-again-fixed response

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 // Sadece test için kullanılmalıdır. 
 // ilerleyen zamanlarda değiştirilmelidir. öpüldünüz...
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:4200' ,'http://localhost:64282'],
+  origin: ['http://localhost:3000', 'http://localhost:4200' ,'http://localhost:52329'],
   credentials: true
 }));
 
