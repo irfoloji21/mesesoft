470e8f46c3a0a56fe8e73a0ebb18e8ac60ac8850
Enver Yigit <108479529+enveryigit21@users.noreply.github.com>
2023-12-01 21:32:10 +0300

    Contact
    Contact

--- frontendAngular/src/app/pages/account/contact/contact.component.ts
+++ frontendAngular/src/app/pages/account/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from 'src/app/shared/services/contact.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor( private formBuilder: FormBuilder , private contactService : ContactService, private toastr: ToastrService, ) {
+    this.contactForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+   })
   }
 
+  ngOnInit(): void {
+    this.contactService.getMessages().subscribe(res =>{
+      console.log(res , "contact")
+    })
+  }
+
+  onSubmit() {
+    const message = this.contactForm.value;
+
+    this.contactService.addMessage(message).subscribe(
+      updatedMessages => {
+        console.log('Message added successfully:', updatedMessages);
+        this.contactForm.reset();
+        this.toastr.success('Address added successfully', 'Success');
+      },
+      error => {
+        console.error('Error adding message:', error);
+        this.toastr.error('An error occurred', 'Error');
+      }
+    );
+  }
 }
---
+++ frontendAngular/src/app/shared/services/contact.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, switchMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private apiUrl = 'http://localhost:3000/messages';
+
+
+
+  constructor(private http: HttpClient) {}
+
+  getMessages(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  addMessage(message: any): Observable<any> {
+    return this.http.post(this.apiUrl, message);
+  }
+}
