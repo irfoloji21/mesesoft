32d08d298cca61832c65f6c5235c05611dfbe0fc
Enver Yigit <108479529+enveryigit21@users.noreply.github.com>
2023-12-10 15:23:08 +0300

    Edit/dashboard/coupon
    Edit/dashboard/coupon

--- frontendAngular/src/app/pages/account/coupon/coupon.component.ts
+++ frontendAngular/src/app/pages/account/coupon/coupon.component.ts
@@ -1,36 +1,72 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { CouponService } from 'src/app/shared/services/coupon.service';
 
 @Component({
-  selector: 'app-coupon',
-  templateUrl: './coupon.component.html',
-  styleUrls: ['./coupon.component.scss']
+  selector: "app-coupon",
+  templateUrl: "./coupon.component.html",
+  styleUrls: ["./coupon.component.scss"],
 })
 export class CouponComponent {
-
-  coupons: any[] = [];
-  public active = 1;
-  constructor(private authService: AuthService, private couponService: CouponService) {}
+  coupons: any = [];
+  public active = 2;
+  isModalOpen: boolean = false;
+  selectedCoupon: any;
+  constructor(
+    private authService: AuthService,
+    private couponService: CouponService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.getCouponss();
-   
+    console.log(this.coupons[0].end_date)
   }
 
-  
-  getCouponss(){
-        this.authService.loadUser().subscribe(res => {
-          for(let a= 0 ; a < res.user.coupons.length ; a++ ) {
-            this.couponService.getCouponById(res.user.coupons[a].couponID).subscribe(res=> {
-              this.coupons[a] = res.couponCode;
-               console.log(this.coupons[a], "İrfanMese")
-              })
-          }
-        })
+  getCouponss() {
+    this.authService.loadUser().subscribe((res) => {
+      for (let a = 0; a < res.user.coupons.length; a++) {
+        this.couponService
+          .getCouponById(res.user.coupons[a].couponID)
+          .subscribe((res) => {
+            this.coupons[a] = res.couponCode;
+            console.log(this.coupons[a], "Coupon");
+          });
       }
+    });
+  }
 
+  openModal(coupon: any) {
+    this.isModalOpen = true;
+    this.selectedCoupon = coupon;
+  }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
 
+  copyCouponCode() {
+    const couponCode = this.selectedCoupon?.name;
+    const textarea = document.createElement("textarea");
+    textarea.value = couponCode || ""; 
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    this.toastr.success("The coupon was copied", "Success");
+    document.body.removeChild(textarea);
 
-    }
+    setTimeout(() => {
+      this.closeModal();
+    }, 1000); 
+  }
+
+  getMonthName(monthNumber: number): string {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+    const monthName = monthNames[monthNumber - 1];
+  
+    return monthName || '';
+  }
+  
+}
