f949f70758896c209420f72abe477e0b8c4a96c6
mahmutmese <sulleymanmese@gmail.com>
2024-01-17 15:09:45 +0300

    savedcart

--- frontendAngular/src/app/shared/services/savedCard.service.ts
+++ frontendAngular/src/app/shared/services/savedCard.service.ts
@@ -1,7 +1,7 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,32 @@ export class SavedCardService {
   constructor(private http: HttpClient) { }
 
   addNewCard(newCard: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/addNewCard`, newCard);
+    const updatedCards = this.getSavedCardsSync();
+    updatedCards.cards.push(newCard);
+
+    // Dosyayı güncelle
+    this.updateSavedCardsFile(updatedCards);
+
+    // Güncellenmiş kartları döndür
+    return of(updatedCards);
+    // return this.http.post(`${this.apiUrl}/addNewCard`, newCard);
   }
 
   getSavedCards(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
+
+  private getSavedCardsSync(): any {
+    // Senkron bir şekilde kartları al (bu örnek için basit bir senaryo)
+    return JSON.parse(localStorage.getItem('savedCards')) || { cards: [] };
+  }
+
+  private updateSavedCardsFile(updatedCards: any): void {
+    // Dosyayı güncelle (bu örnek için basit bir senaryo)
+    localStorage.setItem('savedCards', JSON.stringify(updatedCards));
+  }
+
+  // getSavedCards(): Observable<any> {
+  //   return this.http.get<any>(this.apiUrl);
+  // }
 }
