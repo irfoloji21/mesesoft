b736476c911397bb562f26db7850b08f3ab805ea
Enver Yigit <108479529+enveryigit21@users.noreply.github.com>
2024-02-29 19:30:05 +0300

    Update: ImproveFormSelectionHandling
    Update: ImproveFormSelectionHandling

--- adminAngular/src/app/components/customers/add-customer/add-customer/add-customer.component.ts
+++ adminAngular/src/app/components/customers/add-customer/add-customer/add-customer.component.ts
@@ -1,18 +1,59 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from 'express';
+
 
 @Component({
   selector: 'app-add-customer',
   templateUrl: './add-customer.component.html',
   styleUrls: ['./add-customer.component.scss']
 })
-export class AddCustomerComponent {
+export class AddCustomerComponent implements OnInit {
   buttonText;
+  addCustomerForm: FormGroup
+  selectedOptionGender: string = 'Not Specified';
+  selectedOptionEmailForm: string = 'Main Website';
 
- 
+  constructor(
+    private fb: FormBuilder,
+  ){}
 
+  ngOnInit(): void {
+     this.CustomerForm();
+  }
+
+  CustomerForm(){
+    this.addCustomerForm = this.fb.group({
+      AssociateToWebsite:[''],
+      NamePrefix:    [''],
+      FirstName:     [''],
+      MiddleName:    [''],
+      LastName:      [''],
+      NameSuffix:    [''],
+      Email:         [''],
+      Date:          [''],
+      TaxVatNumber:  [''],
+      VertexCustomer:[''],
+      Gender: ['Not Specified'],
+      EmailForm:['Main Website'],
+
+
+    });
+  }
+  saveForm(): void {
+    const formData = this.addCustomerForm.value;
+    console.log('saveForm:', formData);
+  }
+
+  updateSelectedGender(option: string): void {
+    this.selectedOptionGender = option;
+    this.addCustomerForm.controls['Not Specified'].setValue(option); 
+  }
+
+  updateSelectedEmailForm(option: string): void {
+    this.selectedOptionEmailForm = option;
+    this.addCustomerForm.controls['Main Website'].setValue(option); 
+  }
+  
   
   goBack(){}
-  performAction(){}
 }
